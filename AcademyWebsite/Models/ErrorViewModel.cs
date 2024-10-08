bb98637974a8204709a90f974d2a6167b2d61ae4
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2024-10-08 22:00:25 +0300

    Add project files.

---
+++ AcademyWebsite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AcademyWebsite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
