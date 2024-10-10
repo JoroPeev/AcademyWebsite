7d241706c7535886d432ef321810c5a136c61e0b
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2024-10-10 21:00:15 +0300

    Registration data  model added

---
+++ AcademyWebsite/Models/RegistrationData.cs
@@ -0,0 +1,18 @@
+﻿namespace AcademyWebsite.Models
+{
+    public class RegistrationData
+    {
+        public required int Id { get; set; }
+
+        public required string ChildName { get; set; }
+
+        public required int ChildAge { get; set; }
+
+        public required string ParentName { get; set; }
+
+        public required string PhoneNumber { get; set; }
+
+        public required string EmailAddress { get; set; }
+
+    }
+}
