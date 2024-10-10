4709862b992bc236ed564644ed2c10ab280ac2fe
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2024-10-10 20:59:31 +0300

    Adding DbContext

---
+++ AcademyWebsite/Models/AcademyDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AcademyWebsite.Models
+{
+    public class AcademyDbContext : DbContext
+    {
+        public AcademyDbContext(DbContextOptions<AcademyDbContext> options)
+            : base(options)
+        {
+            
+        }
+        public DbSet<RegistrationData> RegistrationData { get; set; }
+        
+    }
+}
