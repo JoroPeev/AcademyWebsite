b22658953529a9f1f9239d9a831e095b126d2d4d
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2024-10-17 23:58:12 +0300

    Added config for registration

---
+++ AcademyWebsite/SeedData/RegistrationConfigoration.cs
@@ -0,0 +1,18 @@
+﻿using AcademyWebsite.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AcademyWebsite.SeedData
+{
+    internal class RegistrationConfigoration
+    {
+        public void Configure(EntityTypeBuilder<RegistrationData> builder)
+        {
+            var data = new SeedData();
+
+            builder.HasData([data.RegistredUser]);
+        }
+
+
+
+    }
+}
