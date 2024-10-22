3f43f7f6718761da60685c0e1ad0d0e7670de41e
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2024-10-22 23:22:31 +0300

    Added migrations to database

---
+++ AcademyWebsite/Migrations/20241017161345_Initializing.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AcademyWebsite.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initializing : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "RegistrationData",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ChildName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ChildAge = table.Column<int>(type: "int", nullable: false),
+                    ParentName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EmailAddress = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RegistrationData", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RegistrationData");
+        }
+    }
+}
---
+++ AcademyWebsite/Migrations/20241022190551_SeedData.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AcademyWebsite.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "RegistrationData",
+                columns: new[] { "Id", "ChildAge", "ChildName", "EmailAddress", "ParentName", "PhoneNumber" },
+                values: new object[] { 1, 5, "Penka", "JonDoe@gmail.com", "Stanka", "1234567890" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "RegistrationData",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+    }
+}
