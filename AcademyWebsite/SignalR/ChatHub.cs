5689d9fcdcd3052b3bfbc5a6993fe886124a3fff
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2024-11-13 21:20:43 +0200

    Removed old files for signalR added new Hub

---
+++ AcademyWebsite/SignalR/ChatHub.cs
@@ -0,0 +1,11 @@
+﻿namespace AcademyWebsite.SignalR;
+using Microsoft.AspNetCore.SignalR;
+
+public class ChatHub : Hub
+{
+    // Method to send messages to all connected clients
+    public async Task SendMessage(string user, string message)
+    {
+        await Clients.All.SendAsync("ReceiveMessage", user, message);
+    }
+}
