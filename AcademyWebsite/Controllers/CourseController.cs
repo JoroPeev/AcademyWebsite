bbb21c4394bb1c75909f66fe8eae410341956089
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2024-11-21 23:44:32 +0200

    Added course controller

---
+++ AcademyWebsite/Controllers/CourseController.cs
@@ -0,0 +1,15 @@
+﻿using AcademyWebsite.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AcademyWebsite.Controllers
+{
+    public class CourseController(AcademyWebsiteContext context) : Controller
+    {
+        //Primary Constructor
+        private readonly AcademyWebsiteContext _context = context;
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
