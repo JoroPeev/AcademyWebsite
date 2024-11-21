89bfd81cfe5c492af6ec694dcc0071fff283fc41
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2024-11-21 23:44:01 +0200

    Added Course model

---
+++ AcademyWebsite/Models/Course.cs
@@ -0,0 +1,18 @@
+﻿namespace AcademyWebsite.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public decimal Price { get; set; }
+        public string Details { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+
+
+
+
+    }
+}
