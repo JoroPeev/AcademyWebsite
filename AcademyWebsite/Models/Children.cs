c7c1c82ce160a894182040fe125e6ddd81334fcb
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2024-12-02 21:30:30 +0200

    Added children model

---
+++ AcademyWebsite/Models/Children.cs
@@ -0,0 +1,13 @@
+﻿namespace AcademyWebsite.Models
+{
+    public class Children
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string ParentFirstName { get; set; }
+        public string ParentLastName { get; set; }
+        public int ChildAge { get; set; }
+
+    }
+}
