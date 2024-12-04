f5536bf2867117937157eb37c7016aa538f18a5c
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2024-12-04 21:50:49 +0200

    Added foreign key

--- AcademyWebsite/Areas/Identity/Data/AcademyWebsiteContext.cs
+++ AcademyWebsite/Areas/Identity/Data/AcademyWebsiteContext.cs
@@ -16,11 +16,18 @@ public class AcademyWebsiteContext : IdentityDbContext<IdentityUser>
     public DbSet<RegistrationData> RegistrationData { get; set; }
     public DbSet<Message> Messages { get; set; }
     public DbSet<Course> Courses { get; set; }
-
+    public DbSet<Children> Childrens { get; set; }
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
         builder.ApplyConfiguration(new RegistrationConfiguration());
+
+        builder.Entity<Children>()
+         .HasOne(c => c.Course)
+         .WithMany(c => c.Childrens)
+         .HasForeignKey(c => c.CourseId)
+         .OnDelete(DeleteBehavior.Cascade);
+
 
         base.OnModelCreating(builder);
     }
