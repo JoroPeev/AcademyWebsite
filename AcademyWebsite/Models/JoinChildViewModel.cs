f7cda06c3bf290883109e03ce80ced22d67e6f01
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2024-12-05 16:57:20 +0200

    Added join child view model

---
+++ AcademyWebsite/Models/JoinChildViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AcademyWebsite.Models
+{
+    public class JoinChildViewModel
+    {
+        [Required]
+        public int CourseId { get; set; }
+        [Required]
+        public string CourseName { get; set; }
+        [Required]
+        public int CourseAge { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string ParentName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public int ChildAge { get; set; }
+    }
+}
