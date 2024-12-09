36ff62068e4e1a0ca82bcbab83a328c5fd18fdfd
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2024-12-09 21:32:22 +0200

    Maked required properties

--- AcademyWebsite/Models/Children.cs
+++ AcademyWebsite/Models/Children.cs
@@ -1,13 +1,21 @@
-﻿namespace AcademyWebsite.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AcademyWebsite.Models
 {
     public class Children
     {
+        [Required]
         public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string ParentFirstName { get; set; }
-        public string ParentLastName { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string ParentName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
         public int ChildAge { get; set; }
+        public int CourseId { get; set; }
+        public Course Course { get; set; }
 
     }
 }
