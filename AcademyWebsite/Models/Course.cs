d0625ca91f3c89d0e5c733e1bba29f2ee8eca296
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2024-12-10 22:41:16 +0200

    Added imageUrl to model and required attribute

--- AcademyWebsite/Models/Course.cs
+++ AcademyWebsite/Models/Course.cs
@@ -1,17 +1,24 @@
-﻿namespace AcademyWebsite.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AcademyWebsite.Models
 {
     public class Course
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public int Age { get; set; }
+        [Required]
         public decimal Price { get; set; }
+        [Required]
         public string Details { get; set; }
+        public string ImageUrl { get; set; }
+        [Required]
         public DateTime StartDate { get; set; }
+        [Required]
         public DateTime EndDate { get; set; }
-
-
-
+        public List<Children> Childrens { get; set; } = new List<Children>();
 
 
     }
