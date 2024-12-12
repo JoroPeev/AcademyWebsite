f0b0e6e16c7d9d884305a169fd1e3dd6a44c7610
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2024-12-12 22:24:25 +0200

    Added Subject to Course model

---
+++ AcademyWebsite/Migrations/20241212201544_SubjectListAdded.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AcademyWebsite.Migrations
+{
+    /// <inheritdoc />
+    public partial class SubjectListAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Subject",
+                table: "Courses",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Subject",
+                table: "Courses");
+        }
+    }
+}
