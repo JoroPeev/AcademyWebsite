2fcb43f365dfa0042df98672daa413542863ea22
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2024-12-17 16:50:08 +0200

    Added Course service interface

---
+++ AcademyWebsite/Contracts/ICourseService.cs
@@ -0,0 +1,20 @@
+﻿using AcademyWebsite.Models;
+
+namespace AcademyWebsite.Contracts
+{
+    public interface ICourseService
+    {
+        public List<Course> GetAllCourses();
+
+        public Course GetCourseById(int id);
+
+        public void AddCourse(Course course);
+
+        public void UpdateCourse(int id, Course updatedCourse);
+
+        public void DeleteCourse(int id);
+
+        public void AddChildToCourse(Children child);
+    }
+
+}
