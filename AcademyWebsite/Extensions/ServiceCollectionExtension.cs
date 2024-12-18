44d3323eab4882889beed121c655a488cff10138
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2024-12-18 19:31:10 +0200

    Added service collection exstensions

---
+++ AcademyWebsite/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,51 @@
+﻿using AcademyWebsite.Contracts;
+using AcademyWebsite.Data;
+using AcademyWebsite.Services;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+        {
+
+            services.AddScoped<ICourseService, CourseService>();
+
+
+            services.AddSignalR();
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
+            })
+            .AddCookie()
+            .AddGoogle(GoogleDefaults.AuthenticationScheme, options =>
+            {
+                options.ClientId = config["GoogleKeys:ClientId"];
+                options.ClientSecret = config["GoogleKeys:ClientSecret"];
+            });
+
+            return services;
+        }
+
+        public static IServiceCollection AddApplicationDbContext(this IServiceCollection services, IConfiguration config)
+        {
+            var connectionString = config.GetConnectionString("DefaultConnection");
+            services.AddDbContext<AcademyWebsiteContext>(options =>
+                options.UseSqlServer(connectionString));
+            return services;
+        }
+
+        public static IServiceCollection AddApplicationIdentity(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<AcademyWebsiteContext>();
+            return services;
+        }
+    }
+}
