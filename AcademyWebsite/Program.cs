5c381ed2c0661a0167c4b7fc76538a358fced9d6
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2024-12-18 19:29:56 +0200

    Relocated some of the program cs file

--- AcademyWebsite/Program.cs
+++ AcademyWebsite/Program.cs
@@ -1,40 +1,13 @@
-using AcademyWebsite.Data;
 using AcademyWebsite.SignalR;
-using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authentication.Google;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
-
-})
-.AddCookie()
-.AddGoogle(GoogleDefaults.AuthenticationScheme, options =>
-{
-    options.ClientId = builder.Configuration.GetSection("GoogleKeys:ClientId").Value;
-    options.ClientSecret = builder.Configuration.GetSection("GoogleKeys:ClientSecret").Value;
-});
-
-
-
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddApplicationDbContext(builder.Configuration);
+builder.Services.AddApplicationIdentity(builder.Configuration);
 builder.Services.AddControllersWithViews();
 
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<AcademyWebsiteContext>(options =>
-    options.UseSqlServer(connectionString));
-
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<AcademyWebsiteContext>();
-
-builder.Services.AddSignalR();//SignalR
-
-
 var app = builder.Build();
-
 
 if (!app.Environment.IsDevelopment())
 {
@@ -47,18 +20,18 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.UseEndpoints(endpoints =>
 {
     endpoints.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
 
     endpoints.MapRazorPages();
 });
 
-// Map your hub to an endpoint
 app.MapHub<ChatHub>("/chatHub");
 
 app.Run();
