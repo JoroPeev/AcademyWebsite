c3fb87a249a76297d6018f68a063cbda10f502cc
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2024-12-19 21:10:01 +0200

    Added attribute to make redirection on user when is not log in

---
+++ AcademyWebsite/Attributes/RedirectIfNotAuthenticatedAttribute.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+public class RedirectIfNotAuthenticated : ActionFilterAttribute
+{
+    public override void OnActionExecuting(ActionExecutingContext context)
+    {
+        // Check if the user is authenticated
+        if (!context.HttpContext.User.Identity.IsAuthenticated)
+        {
+            // Redirect to the full URL of the Register page
+            context.Result = new RedirectResult("https://localhost:7086/Identity/Account/Register");
+        }
+
+        base.OnActionExecuting(context);
+    }
+}
