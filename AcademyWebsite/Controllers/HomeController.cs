d1e790c746363e2757ff1e8271fba8425e05bfe3
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2024-12-30 17:05:02 +0200

    Removed registrationData and register list from controller

--- AcademyWebsite/Controllers/HomeController.cs
+++ AcademyWebsite/Controllers/HomeController.cs
@@ -1,8 +1,5 @@
 using AcademyWebsite.Data;
-using AcademyWebsite.Models;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
 
 namespace AcademyWebsite.Controllers
 {
@@ -26,83 +23,5 @@ namespace AcademyWebsite.Controllers
             return View();
         }
 
-        public IActionResult RegistrationData()
-        {
-            return View();
-        }
-
-        [RedirectIfNotAuthenticated]
-        public IActionResult RegisteredList()
-        {
-            var allRegistrations = _context.RegistrationData.ToList();
-
-            return View(allRegistrations);
-        }
-
-        public IActionResult RegistrationDetails(RegistrationData model)
-        {
-            var registered = new RegistrationData
-            {
-                Id = model.Id,
-                ChildAge = model.ChildAge,
-                ChildName = model.ChildName,
-                EmailAddress = model.EmailAddress,
-                ParentName = model.ParentName,
-                PhoneNumber = model.PhoneNumber,
-            };
-            _context.Add(registered);
-            _context.SaveChanges();
-
-            return RedirectToAction("RegisteredList");
-        }
-        [RedirectIfNotAuthenticated]
-        public IActionResult EditRegistrationData(int? id)
-        {
-            var datainDb = _context.RegistrationData.SingleOrDefault(ex => ex.Id == id);
-
-
-            return View(datainDb);
-        }
-        [RedirectIfNotAuthenticated]
-        [HttpPost]
-        public IActionResult EditRegistrationData(RegistrationData registration)
-        {
-            if (!ModelState.IsValid)
-            {
-                return View(registration);
-            }
-
-            var model = _context.RegistrationData.SingleOrDefault(ex => ex.Id == registration.Id);
-            if (model == null)
-            {
-                return NotFound();
-            }
-
-            model.PhoneNumber = registration.PhoneNumber;
-            model.ParentName = registration.ParentName;
-            model.EmailAddress = registration.EmailAddress;
-            model.ChildName = registration.ChildName;
-            model.EmailAddress = registration.EmailAddress;
-            model.ChildAge = registration.ChildAge;
-
-            _context.Update(model);
-            _context.SaveChanges();
-
-            return RedirectToAction("RegisteredList");
-        }
-        [RedirectIfNotAuthenticated]
-        public IActionResult DeleteRegistrationData(int id)
-        {
-            var datainDb = _context.RegistrationData.SingleOrDefault(ex => ex.Id == id);
-            _context.RegistrationData.Remove(datainDb);
-            _context.SaveChanges();
-            return RedirectToAction("RegisteredList");
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
