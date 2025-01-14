71e0d9e4236d414feea3bc8ff1924b9215a2a543
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2025-01-14 18:18:05 +0200

    Scaffolded controller in admin area

---
+++ AcademyWebsite/Areas/Admin/Controllers/CourseController.cs
@@ -0,0 +1,161 @@
+﻿using AcademyWebsite.Data;
+using AcademyWebsite.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace AcademyWebsite.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CourseController : Controller
+    {
+        private readonly AcademyWebsiteContext _context;
+
+        public CourseController(AcademyWebsiteContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/Course
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Courses.ToListAsync());
+        }
+
+        // GET: Admin/Course/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var course = await _context.Courses
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (course == null)
+            {
+                return NotFound();
+            }
+
+            return View(course);
+        }
+
+        // GET: Admin/Course/Create
+        public IActionResult Create()
+        {
+            var subjects = new List<string> { "Math", "Science", "Language", "Art" };
+            ViewBag.Subjects = new SelectList(subjects);
+            return View();
+        }
+
+        // POST: Admin/Course/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Age,Price,Subject,Details,ImageUrl,StartDate,EndDate")] Course course)
+        {
+            if (ModelState.IsValid)
+            {
+                var subjects = new List<string> { "Math", "Science", "Language", "Art" };
+                ViewBag.Subjects = new SelectList(subjects);
+                _context.Add(course);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(course);
+        }
+
+        // GET: Admin/Course/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var subjects = new List<string> { "Math", "Science", "Language", "Art" };
+            ViewBag.Subjects = new SelectList(subjects);
+            var course = await _context.Courses.FindAsync(id);
+            if (course == null)
+            {
+                return NotFound();
+            }
+            return View(course);
+        }
+
+        // POST: Admin/Course/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Age,Price,Subject,Details,ImageUrl,StartDate,EndDate")] Course course)
+        {
+            if (id != course.Id)
+            {
+                return NotFound();
+            }
+            var subjects = new List<string> { "Math", "Science", "Language", "Art" };
+            ViewBag.Subjects = new SelectList(subjects);
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(course);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CourseExists(course.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(course);
+        }
+
+        // GET: Admin/Course/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var course = await _context.Courses
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (course == null)
+            {
+                return NotFound();
+            }
+
+            return View(course);
+        }
+
+        // POST: Admin/Course/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var course = await _context.Courses.FindAsync(id);
+            if (course != null)
+            {
+                _context.Courses.Remove(course);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CourseExists(int id)
+        {
+            return _context.Courses.Any(e => e.Id == id);
+        }
+    }
+}
