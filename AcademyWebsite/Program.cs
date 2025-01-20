c266f90a6de38d393fbd774e7866110609284a7d
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2025-01-20 18:32:24 +0200

    Added map route for admin area

--- AcademyWebsite/Program.cs
+++ AcademyWebsite/Program.cs
@@ -26,6 +26,10 @@ app.UseAuthorization();
 app.UseEndpoints(endpoints =>
 {
     endpoints.MapControllerRoute(
+     name: "Admin",
+     pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+    endpoints.MapControllerRoute(
         name: "default",
         pattern: "{controller=Home}/{action=Index}/{id?}");
 
