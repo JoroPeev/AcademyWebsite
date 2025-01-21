70dfcd3827590723ad072310d561cbe3448f8a72
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2025-01-21 18:23:35 +0200

    Removed registration data seed

--- AcademyWebsite/SeedData/SeedData.cs
+++ AcademyWebsite/SeedData/SeedData.cs
@@ -5,7 +5,6 @@ namespace AcademyWebsite.SeedData
     internal class SeedData
     {
 
-        public RegistrationData RegistredUser { get; set; }
 
         public SeedData()
         {
@@ -15,17 +14,6 @@ namespace AcademyWebsite.SeedData
         private void SeedRegistrationData() 
         {
 
-            RegistredUser = new RegistrationData
-            {
-                Id = 1,
-                ChildAge = 5,
-                EmailAddress = "JonDoe@gmail.com",
-                ChildName = "Penka",
-                ParentName = "Stanka",
-                PhoneNumber = "1234567890",
-            };
-        
-        
         }
 
     }
