99db3e7c742f0b4ced6c024005be21a04939445f
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2025-01-21 18:21:55 +0200

    Removed registration data from context

--- AcademyWebsite/Areas/Identity/Data/AcademyWebsiteContext.cs
+++ AcademyWebsite/Areas/Identity/Data/AcademyWebsiteContext.cs
@@ -1,5 +1,4 @@
 ﻿using AcademyWebsite.Models;
-using AcademyWebsite.SeedData;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
@@ -13,14 +12,12 @@ public class AcademyWebsiteContext : IdentityDbContext<IdentityUser>
     {
     }
 
-    public DbSet<RegistrationData> RegistrationData { get; set; }
     public DbSet<Message> Messages { get; set; }
     public DbSet<Course> Courses { get; set; }
     public DbSet<Children> Childrens { get; set; }
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
-        builder.ApplyConfiguration(new RegistrationConfiguration());
 
         builder.Entity<Children>()
          .HasOne(c => c.Course)
