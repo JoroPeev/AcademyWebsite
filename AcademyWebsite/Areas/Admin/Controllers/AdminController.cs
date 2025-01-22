ba69339f879064bc0d700f2cb03536f5e6f118a4
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2025-01-22 22:15:01 +0200

    Added admin controller

---
+++ AcademyWebsite/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+[Authorize(Roles = "Admin")]
+public class AdminController : Controller
+{
+    private readonly UserManager<IdentityUser> _userManager;
+
+    public AdminController(UserManager<IdentityUser> userManager)
+    {
+        _userManager = userManager;
+    }
+
+    public async Task<IActionResult> MakeUserAdmin(string userId)
+    {
+        var user = await _userManager.FindByIdAsync(userId);
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        var result = await _userManager.AddToRoleAsync(user, "Admin");
+        if (result.Succeeded)
+        {
+            return RedirectToAction("Index", "Home");
+        }
+
+        return View("Error");
+    }
+}
