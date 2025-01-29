0d0ff701db98d25f7979b5a9a93d1b0f9e28681b
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2025-01-29 18:21:35 +0200

    Added role service

---
+++ AcademyWebsite/Services/RoleService.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+
+public class RoleService
+{
+    private readonly UserManager<IdentityUser> _userManager;
+    private readonly RoleManager<IdentityRole> _roleManager;
+
+    public RoleService(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+    {
+        _userManager = userManager;
+        _roleManager = roleManager;
+    }
+
+    public async Task AddRoleToUserAsync(string userId, string roleName)
+    {
+        var user = await _userManager.FindByIdAsync(userId);
+        if (user == null)
+        {
+            throw new Exception("User not found");
+        }
+
+        var roleExists = await _roleManager.RoleExistsAsync(roleName);
+        if (!roleExists)
+        {
+            await _roleManager.CreateAsync(new IdentityRole(roleName));
+        }
+
+        var userRoles = await _userManager.GetRolesAsync(user);
+        if (!userRoles.Contains(roleName))
+        {
+            await _userManager.AddToRoleAsync(user, roleName);
+        }
+    }
+}
