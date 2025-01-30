10159e92ee9a7a93009b0c67c92c1c1270067e2d
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2025-01-30 18:02:40 +0200

    Modified role view model

---
+++ AcademyWebsite/Areas/Admin/Models/AddRoleViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+public class AddRoleViewModel
+{
+    [Required]
+    public string RoleName { get; set; }
+
+    [Required]
+    public string SelectedUserId { get; set; }
+
+    public IEnumerable<SelectListItem> Users { get; set; }
+}
