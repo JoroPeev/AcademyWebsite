535efc675847713320d969d053bf91440f9310e5
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2025-02-05 22:05:36 +0200

    Added AssignRoleViewModel

---
+++ AcademyWebsite/Areas/Admin/Models/AssignRoleViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace AcademyWebsite.Areas.Admin.Models
+{
+    public class AssignRoleViewModel
+    {
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public List<SelectListItem> Roles { get; set; }
+    }
+}
