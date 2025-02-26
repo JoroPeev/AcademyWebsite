73b60f82394664e4d0f6947359754598c639a6f9
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2025-02-26 18:03:53 +0200

    Added error controller

---
+++ AcademyWebsite/Controllers/ErrorController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class ErrorController : Controller
+{
+    [Route("Error/404")]
+    public IActionResult NotFoundPage()
+    {
+        Response.StatusCode = 404;
+        return View("NotFound");
+    }
+
+    [Route("Error/500")]
+    public IActionResult InternalServerError()
+    {
+        Response.StatusCode = 500;
+        return View("InternalServerError");
+    }
+
+    [Route("Error/General")]
+    public IActionResult GeneralError()
+    {
+        return View("General");
+    }
+}
