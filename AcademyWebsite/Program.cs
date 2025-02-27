d0edf672e66b01a5ed34697d00939762792d3a42
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2025-02-27 19:20:51 +0200

    Modified to throw custom exeptions

--- AcademyWebsite/Program.cs
+++ AcademyWebsite/Program.cs
@@ -1,3 +1,4 @@
+using AcademyWebsite;
 using AcademyWebsite.Extensions;
 using AcademyWebsite.SignalR;
 using Microsoft.AspNetCore.Identity;
@@ -7,14 +8,22 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddApplicationDbContext(builder.Configuration);
 builder.Services.AddApplicationIdentity(builder.Configuration);
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews(options =>
+{
+    options.Filters.Add(new CustomHandleErrorAttribute());
+});
 
 var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
+    app.UseExceptionHandler("/error/500");
+    app.UseStatusCodePagesWithReExecute("/error/{0}");
     app.UseHsts();
+}
+else
+{
+    app.UseDeveloperExceptionPage();
 }
 
 app.UseHttpsRedirection();
@@ -28,17 +37,16 @@ app.UseAuthorization();
 app.UseEndpoints(endpoints =>
 {
     endpoints.MapControllerRoute(
-     name: "Admin",
-     pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+        name: "Admin",
+        pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
 
     endpoints.MapControllerRoute(
         name: "default",
         pattern: "{controller=Home}/{action=Index}/{id?}");
 
     endpoints.MapRazorPages();
+    endpoints.MapHub<ChatHub>("/chatHub");
 });
-
-app.MapHub<ChatHub>("/chatHub");
 
 using (var scope = app.Services.CreateScope())
 {
@@ -46,4 +54,4 @@ using (var scope = app.Services.CreateScope())
     await RoleSeeder.SeedRolesAsync(roleManager);
 }
 
-app.Run();
+await app.RunAsync();
