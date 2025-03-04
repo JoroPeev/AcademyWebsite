02c43e058f34069f342ade32f06bab3fed70ac00
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2025-03-04 17:38:09 +0200

    Added custom error handler

---
+++ AcademyWebsite/Filters/CustomHandleErrorAttribute.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Net;
+
+namespace AcademyWebsite
+{
+    public class CustomHandleErrorAttribute : ExceptionFilterAttribute
+    {
+        public override void OnException(ExceptionContext context)
+        {
+            if (context.ExceptionHandled || context.HttpContext == null)
+                return;
+
+            var statusCode = context.HttpContext.Response.StatusCode;
+            var viewName = statusCode switch
+            {
+                400 => "BadRequest",
+                401 => "Unauthorized",
+                403 => "Forbidden",
+                404 => "NotFound",
+                _ => "ServerError"
+            };
+
+            context.Result = new ViewResult
+            {
+                ViewName = $"~/Views/Error/{viewName}.cshtml"
+            };
+
+            context.ExceptionHandled = true;
+        }
+    }
+}
