8659cb0283d388fe08af5f1d8a8d49ba715e2531
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2025-03-06 17:52:55 +0200

    Removed comments

--- AcademyWebsite/Attributes/RedirectIfNotAuthenticatedAttribute.cs
+++ AcademyWebsite/Attributes/RedirectIfNotAuthenticatedAttribute.cs
@@ -5,10 +5,8 @@ public class RedirectIfNotAuthenticated : ActionFilterAttribute
 {
     public override void OnActionExecuting(ActionExecutingContext context)
     {
-        // Check if the user is authenticated
         if (!context.HttpContext.User.Identity.IsAuthenticated)
         {
-            // Redirect to the full URL of the Register page
             context.Result = new RedirectResult("https://localhost:7086/Identity/Account/Register");
         }
 
