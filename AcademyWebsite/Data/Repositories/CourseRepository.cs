fe83e30f5d43047e67313601b6711f8f7dfa570e
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2025-03-13 22:37:48 +0200

    Added CourseRepository

---
+++ AcademyWebsite/Data/Repositories/CourseRepository.cs
@@ -0,0 +1,44 @@
+﻿using AcademyWebsite.Data;
+using AcademyWebsite.Models;
+
+public class CourseRepository : ICourseRepository
+{
+    private readonly AcademyWebsiteContext _context;
+
+    public CourseRepository(AcademyWebsiteContext context)
+    {
+        _context = context;
+    }
+
+    public IEnumerable<Course> GetAllCourses()
+    {
+        return _context.Courses.ToList();
+    }
+
+    public Course GetCourseById(int id)
+    {
+        return _context.Courses.Find(id);
+    }
+
+    public void AddCourse(Course course)
+    {
+        _context.Courses.Add(course);
+        _context.SaveChanges();
+    }
+
+    public void UpdateCourse(Course course)
+    {
+        _context.Courses.Update(course);
+        _context.SaveChanges();
+    }
+
+    public void DeleteCourse(int id)
+    {
+        var course = _context.Courses.Find(id);
+        if (course != null)
+        {
+            _context.Courses.Remove(course);
+            _context.SaveChanges();
+        }
+    }
+}
