d15a60ed1b50f80299e95c7beec163e4485bbe47
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2025-03-13 22:37:27 +0200

    Added ICourseRepository

---
+++ AcademyWebsite/Data/Repositories/ICourseRepository.cs
@@ -0,0 +1,11 @@
+﻿
+using AcademyWebsite.Models;
+
+public interface ICourseRepository
+{
+    IEnumerable<Course> GetAllCourses();
+    Course GetCourseById(int id);
+    void AddCourse(Course course);
+    void UpdateCourse(Course course);
+    void DeleteCourse(int id);
+}
